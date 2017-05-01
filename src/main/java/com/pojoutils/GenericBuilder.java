ca268786981a160b33b4cd1ec49f7b18b0cdaa0c
Laptseu <alexlaptseu1@Alexs-MacBook-Pro.local>
2017-05-01 21:55:39 +1000

    imports optimized

--- src/main/java/com/pojoutils/GenericBuilder.java
+++ src/main/java/com/pojoutils/GenericBuilder.java
@@ -2,7 +2,6 @@ package com.pojoutils;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
